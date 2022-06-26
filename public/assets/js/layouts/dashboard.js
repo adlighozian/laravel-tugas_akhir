ee03f90dd1357ff800112ef079adc44a36c29b80
adlighozian <adlighozian1@gmail.com>
2022-06-26 18:18:52 +0700

    Laravel 9

--- public/assets/js/layouts/dashboard.js
+++
@@ -1,7 +0,0 @@
-// SIDEBAR_MENU START
-var menu_btn = document.querySelector("#menu-btn");
-var sidebar = document.querySelector(".sidebarMenu");
-menu_btn.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-});
-// SIDEBAR_MENU END
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.login', [
-        "title" => "TA | Login"
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return view('pages.home', [
-        "title" => "TA | Dashboard"
-    ]);
+    return view('welcome');
 });

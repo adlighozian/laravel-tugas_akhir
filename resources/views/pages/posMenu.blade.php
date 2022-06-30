add8fca64f762401321cecdacb7ed2ef2fb8ad9f
unknown <adelri456@gmail.com>
2022-06-30 16:13:29 +0700

    apa ini

--- resources/views/pages/posMenu.blade.php
+++ resources/views/pages/posMenu.blade.php
@@ -16,7 +16,7 @@
                 <input class="form-control me-2 search-menu rounded" type="search" placeholder="Search" aria-label="Search" name="query">
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
-            <a href="" class="btn btn-success">Category</a>
+            <a href="" class="btn btn-success">Create Category</a>
         </div>
         <p> </p>
         
@@ -31,27 +31,29 @@
         <div class="card mb-3 w-100 h-100">
             <div class="row g-0">
                 <div class="col-md-3 d-flex align-self-center">
-                    <img src="" class="img-menu">
+                    <img src="https://cdn1-production-images-kly.akamaized.net/KxuztQKl3tnUN0Fw5iAwKsnX_u0=/0x148:1920x1230/640x360/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3093328/original/069244600_1585909700-fried-2509089_1920.jpg" 
+                    class="img-menu">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h4 class="card-title">Nasi </h4>
+                        <h5 class="card-title">Nasi Goreng </h5>
+
                         <div class="row menu-desc">
                             <div class="container">
                                 <div class="row">
-                                    <p class="col-sm-2 lh-sm">Description</p>
+                                    <p class="col-sm-2 lh-sm">Description :</p>
                                     <p class="col-sm-10 lh-sm">Aku adalah anak gembala</p>
                                 </div>
                                 <div class="row">
-                                    <p class="col-sm-2 lh-sm">Ingredients</p>
-                                    <p class="col-sm-10 lh-sm">Nasi Goreng</p>
+                                    <p class="col-sm-2 lh-sm">Ingredients :</p>
+                                    <p class="col-sm-10 lh-sm">Nasi</p>
                                 </div>
                                 <div class="row">
-                                    <p class="col-sm-2 lh-lg">Category</p>
+                                    <p class="col-sm-2 lh-lg">Category  :</p>
                                     <p class="col-sm-10 lh-lg">Main Course</p>
                                 </div>
                                 <div class="row">
-                                    <p class="col-sm-2 lh-sm">Price</p>
+                                    <p class="col-sm-2 lh-sm">Price     :</p>
                                     <p class="col-sm-10 lh-sm">Rp. 10.000</p>
                                 </div>
                             </div>

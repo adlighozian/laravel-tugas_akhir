a06bf8c99d3a0990977cf5ee96afd50f1e5f9a2c
adlighozian <adlighozian1@gmail.com>
2022-07-10 20:41:38 +0700

    memperbaiki stage

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,30 +1,27 @@
-@if ($key == null)
-    <nav class=" w-full h-[70px] flex items-center justify-between bg-warnadua px-[20px]">
-        <i id="sidebarbtn" onclick="sidebar()" class='sm:hidden bx bx-menu text-white text-[30px] cursor-pointer'></i>
-        <i id="sidebarbtn" onclick="sidebar2()"
-            class='sm:block hidden bx bx-menu text-white text-[30px] cursor-pointer'></i>
-        <div class="flex">
-            {{-- USENAME_PROFILE START --}}
-            <div class="flex items-end flex-col justify-center text-white">
-                <p>
-                    @if ($user->role == 'admin')
-                        General Manager (Admin)
-                    @elseif ($user->role == 'pos')
-                        Manager Point of Sales
-                    @elseif ($user->role == 'keuangan')
-                        Manager Keuangan
-                    @elseif ($user->role == 'gudang')
-                        Manager Gudang
-                    @endif
-                </p>
-                <p class="text-xs">{{ $user->email }}</p>
-            </div>
-            {{-- USENAME_PROFILE END --}}
-            {{-- PROFILE_IMAGE START --}}
-            <a href="#" class="w-[50px] h-[50px] bg-slate-500 rounded-full overflow-hidden ml-3">
-                <img src="assets/img/profile.png" alt="profile">
-            </a>
-            {{-- PROFILE_IMAGE END --}}
+<nav class=" w-full h-[70px] flex items-center justify-between bg-warnadua px-[20px]">
+    <i id="sidebarbtn" onclick="sidebar()" class='sm:hidden bx bx-menu text-white text-[30px] cursor-pointer'></i>
+    <i id="sidebarbtn" onclick="sidebar2()" class='sm:block hidden bx bx-menu text-white text-[30px] cursor-pointer'></i>
+    <div class="flex">
+        {{-- USENAME_PROFILE START --}}
+        <div class="flex items-end flex-col justify-center text-white">
+            <p>
+                @if ($user->role == 'admin')
+                    General Manager (Admin)
+                @elseif ($user->role == 'pos')
+                    Manager Point of Sales
+                @elseif ($user->role == 'keuangan')
+                    Manager Keuangan
+                @elseif ($user->role == 'gudang')
+                    Manager Gudang
+                @endif
+            </p>
+            <p class="text-xs">{{ $user->email }}</p>
         </div>
-    </nav>
-@endif
+        {{-- USENAME_PROFILE END --}}
+        {{-- PROFILE_IMAGE START --}}
+        <a href="#" class="w-[50px] h-[50px] bg-slate-500 rounded-full overflow-hidden ml-3">
+            <img src="assets/img/profile.png" alt="profile">
+        </a>
+        {{-- PROFILE_IMAGE END --}}
+    </div>
+</nav>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,29 @@
-@extends('main')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('main')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- TAB START --}}
+    <link href="" rel="shortcut icon" />
+    <title><?= $title ?> </title>
+    {{-- TAB END --}}
+    {{-- CDN_CSS START --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+    {{-- CDN_CSS END --}}
+    {{-- CSS START --}}
+    @yield('css')
+    <link rel="stylesheet" href="/assets/css/style.css">
+    {{-- CSS END --}}
+</head>
+
+<body>
     <div class="w-full h-[100vh] flex">
         {{-- IMAGE START --}}
         <div class="sm:block bg-warnatiga w-full h-full hidden duration-150"></div>
@@ -41,4 +64,20 @@
 
         </div>
     </div>
-@endsection
+    {{-- JS_CDN START --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+    <script src=//code.jquery.com/jquery-3.5.1.slim.min.js integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+        crossorigin=anonymous></script>
+    {{-- JS_CDN END --}}
+    {{-- JS START --}}
+    @yield('js')
+    <script src="/assets/js/tailwind.js"></script>
+    <script src="/assets/js/script.js"></script>
+    <script src="/assets/js/gudang.js"></script>
+    {{-- JS END --}}
+</body>
+
+</html>

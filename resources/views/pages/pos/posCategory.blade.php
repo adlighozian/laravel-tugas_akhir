2349d3ed7c71f50d64d89d5fe53c2eb4fda2a49c
adlighozian <adlighozian1@gmail.com>
2022-07-28 13:38:23 +0700

    pos

--- resources/views/pages/pos/posCategory.blade.php
+++ resources/views/pages/pos/posCategory.blade.php
@@ -1,39 +1,50 @@
 @extends('main')
 
 @section('main')
-            {{-- MAIN SATRT --}}
-            <div class="w-full px-3">
-                <div class="w-full flex justify-between items-center h-[80px]">
-                    <p class="font-medium text-lg">Kategori</p>
+    {{-- MAIN SATRT --}}
+    {{-- ALERT_SUCCESS START --}}
+    @include('components.alert')
+    {{-- ALERT_SUCCESS END --}}
+    <div class="w-full sm:h-[70px] h-[50px] bg-white flex items-center px-4 justify-between text-sm">
+        <p class="sm:text-xl font-bold"><a href="/menueditor">Menu Makanan</a> <i class='bx bxs-chevron-right'></i> Tambah
+            category</p>
+    </div>
+    <div class="w-full p-4">
+        <div class="w-full flex flex-col items-start py-3">
+            <form class="sm:mb-0 flex w-fit mb-3" action="/categoryeditor/store" method="POST">
+                @csrf
+                <input class=" rounded-l-md" type="text" name="category_name" placeholder="Tambah Kategori"
+                    aria-label="category_name">
+                <button class="bg-slate-500 rounded-r-md text-white w-fit px-2 font-medium hover:bg-opacity-80"
+                    type="submit">Tambah Kategori</button>
+            </form>
+        </div>
+        <p class="mb-2 font-medium text-xl">Tabel Daftar Category</p>
+        <table class="table table-hover">
+            <thead class="text-white bg-tabelsatu">
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Kategori</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody class="text-black bg-white">
+                <div class="hidden">{{ $no = 1 }}</div>
+                @foreach ($categories as $c)
+                    <tr>
+                        <th scope="row">{{ $no }}</th>
+                        <td>{{ $c->category_name }}</td>
+                        <td class="flex"><a href="/categoryeditor/hapus/{{ $c->id }}"><button
+                                    class="btn btn-danger flex items-center ml-2">Hapus</button></a>
 
-                </div>
-                <div class="w-full flex flex-col items-start py-3">
-                    <form class="sm:mb-0 flex w-full md:w-[300px] mb-3" action="/categoryeditor/store" method="POST">
-                        {{ csrf_field() }}
-                        <input class="form-control rounded-l-md" type="text" name="category_name"
-                            placeholder="Tambah Kategori" aria-label="category_name">
-                        <button class="bg-slate-500 rounded-r-md text-white px-2 font-medium hover:bg-opacity-80"
-                            type="submit">Tambah Kategori</button>
-                    </form>
-                </div>
-                <p class="mb-2 font-medium text-xl">Tabel Daftar User</p>
-                <table class="table table-hover">
-                    <thead class="text-white bg-tabelsatu">
-                        <tr>
-                            <th scope="col">Kategori</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-black bg-white">
-                        @foreach ($categories as $c)
-                            <tr>
-                                <td>{{ $c->category_name }}</td>
-                                <td> <a href="/categoryeditor/hapus/{{ $c->id }}">Hapus</a></td>
-
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            {{-- MAIN END --}}
+                        </td>
+                    </tr>
+                    @php
+                        $no++;
+                    @endphp
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    {{-- MAIN END --}}
 @endsection
--- resources/views/pages/pos/posCreateMenu.blade.php
+++ resources/views/pages/pos/posCreateMenu.blade.php
@@ -5,67 +5,73 @@
     {{-- ALERT_SUCCESS START --}}
     @include('components.alert')
     {{-- ALERT_SUCCESS END --}}
-    <div class="w-full flex items-center flex-col p-3">
-        <p class="text-base font-bold mb-2">Membuat Menu</p>
+    <div class="w-full sm:h-[70px] h-[50px] bg-white flex items-center px-4 justify-between text-sm">
+        <p class="sm:text-xl font-bold"><a href="/menueditor">Menu Makanan</a> <i class='bx bxs-chevron-right'></i> Tambah
+            menu</p>
+    </div>
+    <div class="w-full flex items-center flex-col py-8">
         {{-- FORM_CREATE_MENU START --}}
         <form action="/createmenu/store" method="post" enctype="multipart/form-data"
-            class="sm:w-[570px] w-[350px] flex flex-col">
+            class="lg:w-[800px]  sm:w-[570px] w-[350px] flex flex-col bg-black bg-opacity-10 rounded-xl p-3 shadow-xl">
             @csrf
-            <div class="mb-3">
-                <label for="menuName" class="mb-1 font-medium">Nama Menu</label>
-                <input type="text" id="menuName" class="form-control rounded-2xl h-[48px] border-0" name="name"
-                    value="{{ old('name') }}" required>
-            </div>
-            <div class="mb-3">
-                <label for="description" class="mb-1 font-medium">Description</label>
-                <textarea id="description" class="form-control rounded-2xl h-[100px] border-0" name="description" required> </textarea>
-            </div>
+            <div class="md:flex-row flex flex-col mb-2">
+                <div class="w-full md:pr-2">
+                    <div class="mb-3">
+                        <label for="menuName" class="mb-1 font-medium">Nama Menu</label>
+                        <input type="text" id="menuName" class="form-control rounded-2xl h-[48px] border-0"
+                            name="name" value="{{ old('name') }}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="ingridient" class="mb-1 font-medium">Ingredient</label>
+                        <input type="text" id="Ingridient" class="form-control rounded-2xl h-[48px] border-0"
+                            name="ingredients" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="mb-1 font-medium">Description</label>
+                        <textarea id="description" class="form-control rounded-2xl h-[130px] border-0" name="description" required> </textarea>
+                    </div>
+                </div>
+                <div class="w-full md:pl-2">
+                    <div class="mb-3 flex flex-col">
+                        <label for="inputCategory" class="mb-1 font-medium">Category</label>
+                        <select id="category" class="border-gray-300 rounded-2xl h-[48px] px-2" name="category_id"
+                            required>
+                            <option value="null" hidden>
+                                Pilih Category
+                            </option>
+                            @foreach ($categories as $c)
+                                <option value="{{ $c->id }}">{{ $c->category_name }}</option>
+                            @endforeach
 
-            <div class="mb-3">
-                <label for="ingridient" class="mb-1 font-medium">Ingredient</label>
-                <input type="text" id="Ingridient" class="form-control rounded-2xl h-[48px] border-0" name="ingredients"
-                    required>
-            </div>
-
-            <div class="mb-8 flex flex-col">
-                <label for="inputCategory" class="mb-1 font-medium">Category</label>
-                <select id="category" class="border-gray-300 rounded-2xl h-[48px] px-2" name="category_id" required>
-                    <option value="null" hidden>
-                        Pilih Category
-                    </option>
-                    @foreach ($categories as $c)
-                        <option value="{{ $c->id }}">{{ $c->category_name }}</option>
-                    @endforeach
-
-                </select>
-            </div>
-
-            <div class="mb-1">
-                <label for="inputgambar" class="mb-1 font-medium">Foto</label>
-                <div class="w-full h-[48px] flex items-center">
-                    <input type="file" name="image" id="image" class="form-control border-0 bg-white rounded-md">
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="inputgambar" class="mb-1 font-medium">Foto</label>
+                        <div class="w-full h-[48px] flex items-center">
+                            <input type="file" name="image" id="image"
+                                class="form-control border-0 bg-white rounded-md">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="mb-1 font-medium">Harga</label>
+                        <input type="number" id="price" class="form-control rounded-2xl h-[48px] border-0"
+                            name="price" required>
+                    </div>
+                    <div class="input-form mx-auto">
+                        <label for="price" class="mb-1 font-medium">Sembunyikan menu</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input @error('is_hidden') is-invalid @enderror" type="radio"
+                            name="is_hidden" id="inlineRadio1" value="1">
+                        <label class="form-check-label" for="inlineRadio1">Yes</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input @error('is_hidden') is-invalid @enderror" type="radio"
+                            name="is_hidden" id="inlineRadio2" value="0">
+                        <label class="form-check-label" for="inlineRadio2">No</label>
+                    </div>
                 </div>
             </div>
-
-            <div class="mb-3">
-                <label for="price" class="mb-1 font-medium">Harga</label>
-                <input type="number" id="price" class="form-control rounded-2xl h-[48px] border-0" name="price"
-                    required>
-            </div>
-            <div class="input-form mx-auto">
-                <label class="form-label">Sembunyikan menu?</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input @error('is_hidden') is-invalid @enderror" type="radio" name="is_hidden"
-                    id="inlineRadio1" value="1">
-                <label class="form-check-label" for="inlineRadio1">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input @error('is_hidden') is-invalid @enderror" type="radio" name="is_hidden"
-                    id="inlineRadio2" value="0">
-                <label class="form-check-label" for="inlineRadio2">No</label>
-            </div>
-
             <button type="submit"
                 class="hover:bg-opacity-80 shadow-lg duration-150 w-full h-[48px] bg-warnatiga rounded-2xl text-white font-medium">Create
                 Menu Makanan </button>

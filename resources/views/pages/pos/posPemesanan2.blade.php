70462e49b77de76f238cb8e33a498fa3694106ef
adlighozian <adlighozian1@gmail.com>
2022-08-30 13:32:58 +0700

    memperbaiki tampilan pos

--- resources/views/pages/pos/posPemesanan2.blade.php
+++ resources/views/pages/pos/posPemesanan2.blade.php
@@ -1,72 +1,7 @@
 @extends('main')
 @section('css')
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
-
-    <style>
-        /* .table-bordered.card {
-                border: 0 !important;
-            }
-
-            .card thead {
-                display: none;
-            }
-
-            .card tbody tr {
-                float: left;
-                width: 25em;
-                margin: 0.5em;
-                border: 1px solid #bfbfbf;
-                border-radius: 0.5em;
-                background-color: transparent !important;
-                box-shadow: 0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.25);
-            }
-
-            .card tbody tr td {
-                display: block;
-                border: 0;
-            } */
-        .tbody tr {
-            float: left;
-            width: 19rem;
-            margin: 0.5rem;
-            border: 0.0625rem solid rgba(0, 0, 0, .125);
-            border-radius: .25rem;
-            box-shadow: 0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.25);
-        }
-
-        .tbody td {
-            display: block;
-        }
-
-        .thead {
-            display: none;
-        }
-
-        .td:before {
-            content: attr(data-label);
-            position: relative;
-            float: left;
-            color: #808080;
-            min-width: 4rem;
-            margin-left: 0;
-            margin-right: 1rem;
-            text-align: left;
-        }
-
-        /* tr.selected td:before {
-            color: #CCC;
-        }
-
-        .table .avatar {
-            width: 50px;
-        } */
-
-        .cards .avatar {
-            width: 150px;
-            height: 150px;
-            margin: 15px;
-        }
-    </style>
+    <link rel="stylesheet" href="/assets/css/pos.css">
 @endsection
 
 @section('main')
@@ -78,7 +13,6 @@
     if (!isset($customer_name)) {
         $customer_name = '';
     }
-
     @endphp
     {{-- ALERT_SUCCESS START --}}
     @include('components.alert')
@@ -92,20 +26,9 @@
             </button>
         </div>
     </div>
-    <div class="w-full p-4">
+    <div class="w-full px-[100px] py-4">
         <div class="p-2 bg-black bg-opacity-10 rounded-xl w-full flex items-center flex-col">
-            <div class="w-full flex flex-col items-start py-3">
-                <div class="w-full flex justify-center">
-                    <form action="/pemesanan" class="sm:mb-0 flex w-full md:w-[300px] mb-3" role="search">
-                        @if (request('category'))
-                            <input type="hidden" value="{{ request('category') }}" name="category">
-                        @endif
-                        <input class="form-control rounded-l-md" type="search" value="{{ request('search') }}"
-                            placeholder="Search" name="search">
-                        <button class="bg-slate-500 rounded-r-md text-white px-2 font-medium hover:bg-opacity-80"
-                            type="submit">Search</button>
-                    </form>
-                </div>
+            {{-- <div class="w-full flex flex-col items-start py-3">
                 <div class="flex">
                     <form action="/pemesanan" class="" role="search">
                         @if (request('search'))
@@ -125,24 +48,24 @@
                         </form>
                     @endforeach
                 </div>
-            </div>
+            </div> --}}
             <form action="/checkRequest" class="w-full" method="post">
                 @csrf
                 <button hidden id="submitPesanan"
                     class="sm:text-base p-2 text-xs bg-boxtiga text-white rounded-md flex items-center font-medium hover:bg-opacity-80 duration-150">
                     <i class='bx bx-plus-medical mr-2'></i>Pesan Makanan
                 </button>
-                <div class="sm:mb-0 flex w-full md:w-[300px] mb-3" role="tableNumber">
+                <div class="sm:mb-0 flex w-full mb-3 justify-center" role="tableNumber">
                     @if ($table_number != '')
                         <div class="mb-3 flex w-fit">
                             <div class="mr-2">
                                 <label for="tableNumber" class="mb-1 font-medium">Nomor Meja</label>
-                                <input type="number" id="tableNumber" class="form-control rounded-2xl h-[48px] border-0"
+                                <input type="number" id="tableNumber" class="form-control rounded-2xl h-[30px] border-0"
                                     name="tableNumber" required value="{{ $table_number }}" readonly>
                             </div>
                             <div>
                                 <label for="customerName" class="mb-1 font-medium">Nama Pemesan</label>
-                                <input type="text" id="customerName" class="form-control rounded-2xl h-[48px] border-0"
+                                <input type="text" id="customerName" class="form-control rounded-2xl h-[30px] border-0"
                                     name="customerName" required value="{{ $customer_name }}" readonly>
                             </div>
                             <input type="text" name="kode_order" value="{{ $kode_order }}">
@@ -162,21 +85,23 @@
                         </div>
                     @endif
                 </div>
-                <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                    <thead>
+                {{-- <div class="px-[100px]"> --}}
+                <table id="example" class="table table-striped table-bordered w-fit" cellspacing="0" width="100%">
+                    <thead class="text-white bg-tabelsatu mt-5">
                         <tr>
-                            <th>Checked</th>
-                            <th>Nama Makanan</th>
-                            <th>Deskripsi Makanan</th>
-                            <th>Gambar</th>
-                            <th>Harga</th>
-                            <th>Total</th>
+                            <th hidden>Checked</th>
+                            <th hidden>Nama Makanan</th>
+                            <th hidden>Deskripsi Makanan</th>
+                            <th>Menu</th>
+                            <th class="text-center">Kategori</th>
+                            <th class="text-center">Harga</th>
+                            <th class="text-center">Total</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="bg-white">
                         @foreach ($menu as $m)
                             <tr>
-                                <td>
+                                <td hidden>
                                     @if ($m->is_hidden === 0)
                                         <div class="form-check hidden">
                                             <input class="form-check-input mx-auto" type="checkbox"
@@ -185,36 +110,54 @@
                                         </div>
                                     @endif
                                 </td>
-                                <td>{{ $m->name }}</td>
-                                <td>{{ $m->description }}</td>
-                                <td>
-                                    @if ($m->image)
-                                        <img src="{{ asset('storage/' . $m->image) }}" alt="">
-                                    @else
-                                        <img src="{{ asset('assets/img/empty.png') }}" alt="">
-                                    @endif
+                                <td hidden>{{ $m->description }}</td>
+                                <td hidden>{{ $m->name }}</td>
+                                <td class="p-0">
+                                    <div class="flex">
+                                        <div
+                                            class="mr-2 overflow-hidden w-[200px] h-[200px] flex justify-center items-center">
+                                            @if ($m->image)
+                                                <img src="{{ asset('storage/' . $m->image) }}" alt="">
+                                            @else
+                                                <img class="border-2 border-black" src="{{ asset('assets/img/empty.png') }}"
+                                                    alt="">
+                                            @endif
+                                        </div>
+                                        <div>
+                                            <div class="font-bold text-xl mb-3">
+                                                {{ $m->name }}
+                                            </div>
+                                            <div class="w-[300px]">
+                                                {{ $m->description }}
+                                            </div>
+                                        </div>
+                                    </div>
                                 </td>
-                                <td>Rp.{{ number_format($m->price) }}</td>
-                                <td>
+                                <td class="text-center justify-center" style="vertical-align: middle;">{{ $m->category }}
+                                </td>
+                                <td class="text-center" style="vertical-align: middle;">
+                                    Rp.{{ number_format($m->price) }}
+                                </td>
+                                <td style="vertical-align: middle;">
                                     @if ($m->is_hidden === 1)
                                         <div class="flex p-2">
-                                            <button type="button" class="btn btn-secondary"
-                                                disabled="disabled">-</button>
+                                            <button type="button" class="btn btn-secondary" disabled="disabled">+</button>
                                             <input type="text" name="total[{{ $m->id }}]"
                                                 class="form-control text-center  font-medium " disabled value="Stok habis"
                                                 min="0" max="100">
-                                            <button type="button" class="btn btn-secondary"
-                                                disabled="disabled">+</button>
+                                            <button type="button" class="btn btn-secondary" disabled="disabled">-</button>
                                         </div>
                                     @else
                                         <div class="flex p-2">
-                                            <button type="button" class="btn btn-secondary btn-number btn-pesan-number"
+                                            <button type="button"
+                                                class="btn btn-secondary text-black btn-number btn-pesan-number"
                                                 disabled="disabled" data-type="minus"
                                                 data-field="total[{{ $m->id }}]">-</button>
                                             <input type="text" name="total[{{ $m->id }}]"
                                                 class="form-control input-number text-center pesan-number" value="0"
                                                 min="0" max="100">
-                                            <button type="button" class="btn btn-secondary btn-number btn-pesan-number"
+                                            <button type="button"
+                                                class="btn btn-secondary text-black btn-number btn-pesan-number"
                                                 data-type="plus" data-field="total[{{ $m->id }}]">+</button>
                                         </div>
                                     @endif
@@ -223,6 +166,7 @@
                         @endforeach
                     </tbody>
                 </table>
+                {{-- </div> --}}
             </form>
         </div>
     </div>
@@ -232,15 +176,5 @@
 
 @section('js')
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
-    <script>
-        $(document).ready(function() {
-            //Only needed for the filename of export files.
-            //Normally set in the title tag of your page.
-            document.title = "Card View DataTable";
-            // DataTable initialisation
-            $("#example").DataTable({
-
-            });
-        });
-    </script>
+    <script src="/assets/js/pos.js"></script>
 @endsection
